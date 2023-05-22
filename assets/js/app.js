2dd9516bad95983247850815f0e381e5a998a1a2
YohannHommet <yohann.hommet@outlook.fr>
2023-05-22 22:41:22 +0200

    :recycle: refactoring app module

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,30 @@
 const app = {
-    // Images pour le wall
-    img: Array.from(Array(42).keys()),
+    maxThumbs: 12,
+    imagesLength: Array.from(Array(42).keys()),
     displayedImg: [],
     newWallButton: document.querySelector("#newWallButton"),
+    thumbnail: document.querySelector(".thumbnail"),
 
     init: () => {
         tilt.init();
         form.init();
 
-        // Update nv style on scroll
+        // Update nav style on scroll
         window.addEventListener('scroll', function () {
             const navbar = document.querySelector('.navbar')
             navbar.classList.toggle('sticky', window.scrollY > 0)
         });
 
         app.newWallButton.addEventListener('click', app.newWall);
-
         app.generateThumbnails();
     },
 
     /**
      * Create a new wall on button click
+     * @returns {void}
      */
     newWall: () => {
-        const thumbnail = document.querySelector(".thumbnail");
+        const { thumbnail } = app;
 
         while (thumbnail.firstChild) {
             thumbnail.removeChild(thumbnail.lastChild);
@@ -33,46 +34,61 @@ const app = {
     },
 
     /**
-     * Create a new wall every page refresh
+     * Create a new wall on button click
+     * @returns {void}
      */
     generateThumbnails: () => {
-        const thumbnail = document.querySelector(".thumbnail");
-
-        const maxThumbs = 12;
-        let numbers = [];
-        let n, p;
+        const { maxThumbs, thumbnail } = app;
+        const numbers = new Set();
 
         for (let i = 0; i < maxThumbs; i++) {
-            app.displayedImg[i] = document.createElement("img");
-
-            // Generate random list of numbers
+            let randomNumber;
+        
             do {
-                n = Math.floor(Math.random() * app.img.length);
-                p = numbers.includes(n);
+              randomNumber = app.getRandomNumber();
+            } while (numbers.has(randomNumber));
+        
+            numbers.add(randomNumber);
+            const thumbnailImg = app.createThumbnail(randomNumber);
+            thumbnail.appendChild(thumbnailImg);
+          }
 
-                if (!p) {
-                    numbers.push(n);
-                    // Add src with img fallback for IE
-                    app.displayedImg[i].src = "/assets/img/cats/sm_" + n + ".jpg";
-                    //  Add attributes
-                    app.displayedImg[i].setAttribute('data-tilt-' + n, '');
-                    app.displayedImg[i].alt = "Cat image n°" + n;
-                    app.displayedImg[i].classList.add('thumb');
-                }
-            } while (p);
+        app.initVanillaTilt();
+    },
 
-            thumbnail.appendChild(app.displayedImg[i]);
-        }
+    /**
+     * @returns {number}
+     **/
+    getRandomNumber: () => {
+        return Math.floor(Math.random() * app.imagesLength.length);
+    },
+    
+    /**
+     * @param {number} number 
+     * @returns {HTMLImageElement}
+     */
+    createThumbnail: (number) => {
+        const thumbnailImg = document.createElement("img");
+        const src = `/assets/img/cats/sm_${number}.jpg`;
+    
+        // Set attributes
+        thumbnailImg.src = src;
+        thumbnailImg.setAttribute(`data-tilt-${number}`, '');
+        thumbnailImg.alt = `Cat image n°${number}`;
+        thumbnailImg.classList.add('thumb');
+    
+        return thumbnailImg;
+    },
 
-        // Init VanillaTilt
+    initVanillaTilt: () => {
         VanillaTilt.init(document.querySelectorAll(".thumb"), {
             max: 25,
             speed: 400,
             glare: true,
-            "max-glare": 0.8,
+            "max-glare": 0.5,
             axis: 'x',
         });
-    },
+    }
 }
 
 // Onclick function
